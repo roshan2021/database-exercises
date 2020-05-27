53f785f1f2504bd20eca3bf79710b4b40b3340ed
Roshan Jacob <roshan.jacob2021@gmail.com>
2020-05-27 00:24:55 -0500

    add sql to tables exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,2 +1,13 @@
 USE codeup_test_db;
 
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(100) NOT NULL,
+    name VARCHAR (100) NOT NULL,
+    release_date INT NOT NULL,
+    sales DECIMAL(6, 3) NOT NULL,
+    genre VARCHAR (100),
+    PRIMARY KEY (id)
+);
